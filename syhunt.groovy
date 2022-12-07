ffedf62444f0ba5e38d97e0b0ea7f2a16c938e48
Roberto Marc <roberto@syhunt.com>
2022-12-07 17:52:20 -0300

    Update syhunt.groovy

--- syhunt.groovy
+++ syhunt.groovy
@@ -118,7 +118,7 @@ def scanURL(Map m) {
   if (m.genRep == false) { genRep = m.genRep }
       echo "Preparing to scan URL: $target"
   if (isUnix()) {
-      sh "su ubuntu -c '$cmd $target -hm:$huntMethod -nv -gr -rout:$output'"
+      sh "sudo -u ubuntu -c '$cmd $target -hm:$huntMethod -nv -gr -rout:$output' ubuntu"
   } else {      
       def cmdline = "$cmd $target -hm:$huntMethod -nv -gr -rout:$output"
       print cmdline.execute().text
